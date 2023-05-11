462f90c5b9b6c5f574670f7597f08cc3c0bc7182
AbenezerDaniell <abenezeredu0@gmail.com>
2023-05-12 00:13:46 +0300

    finish the main part

--- js/grid.js
+++ js/grid.js
@@ -1,20 +1,19 @@
 // implement collapsible navbar for mobile design
-const navToggler = document.querySelector('.nav__toggler');
-console.log(navToggler)
-const navList = document.querySelector('.nav__list');
-const checkBox = document.querySelector('#checkbox');
-const togglerIcon = document.querySelector('.toggler__icon')
+const navToggler = document.querySelector(".nav__toggler");
+console.log(navToggler);
+const navList = document.querySelector(".nav__list");
+const checkBox = document.querySelector("#checkbox");
+const togglerIcon = document.querySelector(".toggler__icon");
 
-
-navToggler.addEventListener('click', e => {
-    navList.classList.toggle('nav__expand');
+navToggler.addEventListener("click", (e) => {
+  navList.classList.toggle("nav__expand");
 });
 
-checkBox.addEventListener('change', e => {
-    document.body.classList.toggle('dark-mode');
-    console.log(document.body.classList);
-})
+checkBox.addEventListener("change", (e) => {
+  document.body.classList.toggle("dark-mode");
+  console.log(document.body.classList);
+});
 
-
+// Image full screen mode
 
 
